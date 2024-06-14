64a5f104d2dd07512383a698c60e07ec17e6d853
manikantha939 <manikanthaambig66@gmail.com>
2024-06-14 21:56:30 +0530

    commit from develop branch

---
+++ src/test/java/Program100/Pattern/GitTest.java
@@ -0,0 +1,15 @@
+package Program100.Pattern;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+    @Test
+    public void test1(){
+        System.out.println("Develop branch: Test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Develop branch: Test 2");
+    }
+}
